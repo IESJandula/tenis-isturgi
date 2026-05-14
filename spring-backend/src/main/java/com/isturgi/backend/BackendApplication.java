c980423554f4a43858bcb4c777f5279060cde0ec
Juanca-309 <jbalalv974@g.educaand.es>
2026-05-14 23:24:50 +0200

    Solucion nuclear: exclude security auto-config v6

--- spring-backend/src/main/java/com/isturgi/backend/BackendApplication.java
+++ spring-backend/src/main/java/com/isturgi/backend/BackendApplication.java
@@ -2,15 +2,15 @@ package com.isturgi.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration; // Importante
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration; // Importante
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(exclude = { 
     SecurityAutoConfiguration.class, 
     UserDetailsServiceAutoConfiguration.class 
 })
-@Import(SecurityConfig.class)
+@Import(SecurityConfig.class) // Forzamos tu archivo
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
