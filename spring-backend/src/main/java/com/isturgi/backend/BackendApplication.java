df21077f61cd0875f9d54c21b96cd0aa56ddaecb
Juanca-309 <jbalalv974@g.educaand.es>
2026-05-14 23:06:13 +0200

    Forzando carga de SecurityConfig v5

--- spring-backend/src/main/java/com/isturgi/backend/BackendApplication.java
+++ spring-backend/src/main/java/com/isturgi/backend/BackendApplication.java
@@ -3,10 +3,11 @@ package com.isturgi.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity
+@Import(SecurityConfig.class)
 public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
