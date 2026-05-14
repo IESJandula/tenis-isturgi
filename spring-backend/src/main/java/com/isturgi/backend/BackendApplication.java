d94e4d6204e71b1c509d357dacfd0f1c0011f311
Juanca-309 <jbalalv974@g.educaand.es>
2026-05-14 23:16:27 +0200

    Excluyendo autoconfiguracion de seguridad

--- spring-backend/src/main/java/com/isturgi/backend/BackendApplication.java
+++ spring-backend/src/main/java/com/isturgi/backend/BackendApplication.java
@@ -2,11 +2,14 @@ package com.isturgi.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration; // Importante
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration; // Importante
 import org.springframework.context.annotation.Import;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { 
+    SecurityAutoConfiguration.class, 
+    UserDetailsServiceAutoConfiguration.class 
+})
 @Import(SecurityConfig.class)
 public class BackendApplication {
     public static void main(String[] args) {
